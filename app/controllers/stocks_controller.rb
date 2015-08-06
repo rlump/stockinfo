faf702ed4ba6c13cd8d79b2e2fd28462466db0d9
Ronald Lump <rlump@ameritech.net>
2015-08-06 10:51:16 -0500

    massage data for ember

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -10,23 +10,37 @@ class StocksController < ApplicationController
       @stocks = Stock.all
     end
 
+    # placate ember data's need for a root
+    @stocksHash = {}
+    @stocksHash["stocks"] = @stocks
+
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @stocks }
+      format.json { render json: @stocksHash }
     end
   end
 
   def price_history
 
     if (params[:symbol])
+      id = 0
       @price_hist = YahooFinance.historical_quotes(params[:symbol], \
         { start_date: Time::now-(24*60*60*30), end_date: Time::now }). \
-        collect {|x| OpenStruct.new(x).marshal_dump }
+        collect {|x|
+          pHash = OpenStruct.new(x).marshal_dump
+          id += 1 # placate ember data's demand for an id
+          pHash[:id] = id
+          pHash
+        }
     end
+
+    # placate ember data's need for a root
+    @priceHash = {}
+    @priceHash["prices"] = @price_hist
 
     respond_to do |format|
       format.html # stock_history.html.erb
-      format.json { render json: @price_hist }
+      format.json { render json: @priceHash }
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,6 @@ Rails.application.routes.draw do
 
   get 'stocks' => 'stocks#index'
   get 'price_history' => 'stocks#price_history'
+  get 'prices' => 'stocks#price_history'
   get 'price_history_avg' => 'stocks#price_history_avg'
 end
